b1996bb6f70900f8b4b89c49b5f08c97b7f25bc7
MikeDev0X <mike0vortex@gmail.com>
2023-07-11 23:56:27 -0600

    Authentication in login implemented

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,32 +1,88 @@
-import logo from '../assets/logo.png'
-import Button from '../components/Buttons'
-import styles from './LogIn.module.css'
+import { useState } from 'react';
+import logo from '../assets/logo.png';
+import Button from '../components/Buttons';
+import styles from './LogIn.module.css';
+import {urlLocal} from '../../constants';
 
 function LogIn() {
+
+  const [admin, setAdmin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleAdmin = (e) => {
+    setAdmin(e.target.value);
+  };
+
+  const handlePassword = (e) =>{
+    setPassword(e.target.value);
+  }
+
+  const handleSubmit = async (e) =>{
+
+    e.preventDefault();
+
+    let formData = JSON.stringify({
+      "name": admin,
+      "password": password,
+    });
+
+    fetch(urlLocal + 'login/', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: formData,
+    })
+    .then((response) => response.json())
+    .then((data) =>{
+
+      if(data){
+
+        console.log(data.mensaje);
+
+        if(data.mensaje !== 'login failed'){
+          localStorage.setItem('token', data.token);
+        }
+        else{
+          // login failed modal
+        }
+
+      }
+    })
+
+  }
+
   return (
     <div className={styles.grid}>
+
       <header className={styles.header}>
         <img src={logo} alt="Logo" />
       </header>
+
       <div className={styles.logIn}>
+
         <h1>Inicio de sesión</h1>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+
           <fieldset className={styles.formGroup}>
             <label htmlFor="user">Usuario</label>
-            <input type="text" id='user' name="user" placeholder="leomjs9774" required />
+            <input type="text" id='user' name="user" placeholder="leomjs9774" onChange={handleAdmin} required />
           </fieldset>
 
           <fieldset className={styles.formGroup}>
             <label htmlFor="password">Contraseña</label>
-            <input type="password" id='password' name="password" placeholder="••••••••" required />
+            <input type="password" id='password' name="password" placeholder="••••••••" onChange={handlePassword} required />
           </fieldset>
 
-          <Button type="submit" text="Iniciar sesión" variant="default" />
+          <Button type="submit" text="Iniciar sesión" variant="default"/>
+
         </form>
       </div>
+
       <footer>
         <span>Copyright© 2023 Company. Todos los derechos reservados.</span>
       </footer>
+
     </div>
   )
 }
